805984e819c12588bdd2130316d9957be5f44816
Benito <Benito0727@yandex.ru>
2023-05-31 15:04:15 +0300

    add genre and rating

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -25,6 +25,10 @@ public class Film {
 
     private Set<Integer> likes = new HashSet<>(); // лайки фильма
 
+    private Set<Enum<Genre>> genres = new HashSet<>();
+
+    private Enum<Rating> rating;
+
     public void setLikes(int userId) {
         likes.add(userId);
 
@@ -32,5 +36,53 @@ public class Film {
 
     public void removeLike(int userId) {
         likes.remove(userId);
+    }
+
+
+    public void addGenre(String genre) {
+
+        switch (genre) {
+            case "Comedy" :
+                genres.add(Genre.COMEDY);
+                break;
+            case "Drama" :
+                genres.add(Genre.DRAMA);
+                break;
+            case "Cartoon" :
+                genres.add(Genre.CARTOON);
+                break;
+            case "Thriller" :
+                genres.add(Genre.THRILLER);
+                break;
+            case "Documentary" :
+                genres.add(Genre.DOCUMENTARY);
+                break;
+            case "Action" :
+                genres.add(Genre.ACTION);
+                break;
+        }
+    }
+
+    public void removeGenre(String genre) {
+        switch (genre) {
+            case "Comedy" :
+                genres.remove(Genre.COMEDY);
+                break;
+            case "Drama" :
+                genres.remove(Genre.DRAMA);
+                break;
+            case "Cartoon" :
+                genres.remove(Genre.CARTOON);
+                break;
+            case "Thriller" :
+                genres.remove(Genre.THRILLER);
+                break;
+            case "Documentary" :
+                genres.remove(Genre.DOCUMENTARY);
+                break;
+            case "Action" :
+                genres.remove(Genre.ACTION);
+                break;
+        }
     }
 }
